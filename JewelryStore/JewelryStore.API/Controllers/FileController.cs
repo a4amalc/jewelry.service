c4dd177aecfbed965515a5c1aefde0dd04907202
Amal Chalil <amal.chalil@grit-stone.com>
2021-02-17 16:32:32 +0530

    Functionalities completed. Code optimization pending

---
+++ JewelryStore/JewelryStore.API/Controllers/FileController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using JewelryStore.Business;
+using JewelryStore.Business.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SelectPdf;
+
+namespace JewelryStore.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        private readonly ILogger<FileController> _logger;
+
+        public FileController(ILogger<FileController> logger, IAuthService authService)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("ExportPdf")]
+        public IActionResult ExportToPdf(ExportDto exportDto)
+        {
+            HtmlToPdf converter = new HtmlToPdf();
+            exportDto.FileContent = "<h2>JEWELRY STORE</><table _ngcontent-ng-cli-universal-c0=\"\" border=\"1\" style=\"border-collapse: collapse; width: 100%; height: 14px;\"><tbody _ngcontent-ng-cli-universal-c0=\"\"><tr _ngcontent-ng-cli-universal-c0=\"\" style=\"height: 14px;\"><td _ngcontent-ng-cli-universal-c0=\"\" style=\"width: 25%; height: 14px;\">Sl No</td><td _ngcontent-ng-cli-universal-c0=\"\" style=\"width: 25%; height: 14px;\">Price</td><td _ngcontent-ng-cli-universal-c0=\"\" style=\"width: 25%; height: 14px;\">Weight</td><td _ngcontent-ng-cli-universal-c0=\"\" style=\"width: 12.5%; height: 14px;\">Discount(%)</td><td _ngcontent-ng-cli-universal-c0=\"\" style=\"width: 12.5%; height: 14px;\">Total</td></tr><tr _ngcontent-ng-cli-universal-c0=\"\"><td _ngcontent-ng-cli-universal-c0=\"\" style=\"width: 25%;\">&nbsp;" + "1" + "</td><td _ngcontent-ng-cli-universal-c0=\"\" style=\"width: 25%;\">&nbsp;" + exportDto.Price + "</td><td _ngcontent-ng-cli-universal-c0=\"\" style=\"width: 25%;\">&nbsp;" + exportDto.Weight + "</td><td _ngcontent-ng-cli-universal-c0=\"\" style=\"width: 12.5%;\">&nbsp;" + exportDto.Discount + "</td><td _ngcontent-ng-cli-universal-c0=\"\" style=\"width: 12.5%;\">&nbsp;" + exportDto.Total + "</td></tr></tbody></table>";
+            PdfDocument doc = converter.ConvertHtmlString(exportDto.FileContent);
+            var content = doc.Save();
+            return File(content, "application/pdf");
+        }
+
+        [HttpGet("Print")]
+        public IActionResult Print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JewelryStore/JewelryStore.Business/JewelryStore.Business/DTO/ExportDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JewelryStore.Business
+{
+    public class ExportDto
+    {
+        public string FileContent { get; set; }
+        public string Price { get; set; }
+        public string Weight { get; set; }
+        public string Discount { get; set; }
+        public string Total { get; set; }
+        public string FileName { get; set; }
+    }
+}
